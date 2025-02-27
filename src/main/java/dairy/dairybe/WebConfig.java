889baab3fbca1d12a4d86a8c677b4c16cb22b321
hyunsik <hora5225@gmail.com>
2025-02-28 02:22:55 +0900

    webconfig cors 변경

--- src/main/java/dairy/dairybe/WebConfig.java
+++ src/main/java/dairy/dairybe/WebConfig.java
@@ -12,9 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 허용할 프론트엔드 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 쿠키 포함 요청 허용
+                .allowedOrigins("https://mydairy.my") // 클라우드용
+                //.allowedOrigins("http://localhost:3000") // 허용할 프론트엔드 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
